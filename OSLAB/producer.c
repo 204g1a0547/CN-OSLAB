baf3a705b7f5a643cea6a37447767fd5ad7883de
204g1a0547 <94514824+204g1a0547@users.noreply.github.com>
2022-11-17 15:07:42 +0530

    Lalitha created producer.c

---
+++ OSLAB/producer.c
@@ -0,0 +1,6 @@
+/* Name Of the Candidate: R.Lalitha
+   Roll No: 204G1A0547
+   Title of the Experiment: To simulate producer-consumer problem using semaphores.
+   Date of Creation: 17-11-2022
+   Date of Execution: 17-11-2022
+*/
